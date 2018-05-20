cda9ab29fae519b13c7fc47f7d29ee54467b30a4
abhay <abhayagarwal94@gmail.com>
2018-05-20 17:09:46 +0530

    Home DashBoard implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../shared/user.service';
 import {LoginComponent} from '../login/login.component';
+import {SafeStyle, DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -9,14 +10,37 @@ import {LoginComponent} from '../login/login.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private userService: UserService, private loginComponent: LoginComponent) { }
+   public bgImagePath: SafeStyle;
+    rawImagePath: string;
+    inventoryImagePath: string;
+    billingImagePath: string;
+    transportationImagePath: string;
+    receiptImagePath: string;
+    challanImagePath: string;
+    voucherImagePath: string;
+    dispatchImagePath: string;
+
+  constructor(private userService: UserService, private loginComponent: LoginComponent,private sanitization:DomSanitizer) {
+
+      this.rawImagePath = 'assets/img/raw.jpg';
+      this.inventoryImagePath = 'assets/img/inventory.jpg';
+      this.billingImagePath = 'assets/img/bills.jpeg';
+      this.transportationImagePath = 'assets/img/trans.png';
+      this.receiptImagePath = 'assets/img/receipt.png';
+      this.challanImagePath = 'assets/img/challan.png';
+      this.voucherImagePath = 'assets/img/voucher.png';
+      this.dispatchImagePath = 'assets/img/dispatch.png';
+  }
+
 
   ngOnInit() {
+      this.bgImagePath = this.sanitization.bypassSecurityTrustStyle( 'assets/img/bg.jpeg');
   }
 
   logout() {
     this.userService.logout() ;
     this.loginComponent.resetForm();
   }
+
 
 }
