ab5fda0d5abd8a39f012b74667ea1d8f36446eb4
utkarsh <utkarshd80@gmail.com>
2018-05-31 02:44:12 +0530

    Bill created. Persisted to DB.

--- src/app/shared/salesbill.model.ts
+++ src/app/shared/salesbill.model.ts
@@ -1,13 +1,16 @@
 import {OrderModel} from './order.model';
+import {Item} from './item.model';
 
 export class SalesbillModel {
 
-    constructor(type: string, voucher: string) {
+    constructor(type: string, voucher: string, items: Array<Item>) {
             this.type = 'Finish Sales (GST)';
             this.voucher = voucher;
+            this.items = items;
     }
 
     type: string;
     voucher: string;
+    items: Array<Item>;
 
 }
--- src/app/shared/salesbill.service.ts
+++ src/app/shared/salesbill.service.ts
@@ -4,6 +4,10 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {ISalesBill} from './salesBill';
 import {SalesbillModel} from './salesbill.model';
+import {Item} from './item.model';
+import {SalesBillComponent} from '../sales-bill/sales-bill.component';
+import {SalesBillHomeComponent} from '../sales-bill-home/sales-bill-home.component';
+import {ItemService} from './item.service';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -12,19 +16,24 @@ const httpOptions = {
 @Injectable()
 export class SalesbillService {
 
-    constructor(private router: Router, private http: HttpClient) { }
+    constructor(private router: Router, private http: HttpClient, private itemService: ItemService) { }
 
 
-    createBill(bill: SalesbillModel) {
+    createBill(bill: SalesbillModel, items: Array<Item>) {
         bill.type = 'Finish Sales (GST)';
         console.log(bill.type);
         console.log('done with it');
+        bill.items = items;
+        const uri = 'http://localhost:4000/salesbill/add';
+        console.log(bill);
+        const ans = this.http.post(uri, bill).subscribe(res => console.log('Done'));
+        console.log('Did the http call');
         this.router.navigate(['/sales-home']);
     }
 
-    getBills(): Observable<ISalesBill[]> {
+    getBills(): Observable<SalesbillModel[]> {
         const uri = 'http://localhost:4000/salesbill';
-        const ans = this.http.get<ISalesBill[]>(uri);
+        const ans = this.http.get<SalesbillModel[]>(uri);
         console.log(ans);
         return ans;
     }
